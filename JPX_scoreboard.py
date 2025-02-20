1f6f7be60ed66be953306f7bfd4838c87559471c
Samuel Ficke <samuel.ficke@gmail.com>
2025-02-20 17:20:35 -0500

    lots of updates

--- JPX_scoreboard.py
+++ JPX_scoreboard.py
@@ -2,50 +2,67 @@ from tkinter import *
 from random import *
 import time
 import datetime
-import pyttsx3
+import pyttsx3 # text to speech
+from playsound import playsound
 import math
 from PIL import Image, ImageTk
 
 # score needed to win
 winning_score = 11
 
-# scale factor for displaying on different sized screens
-scale = 1.5
-
 tit_font      = "Harry P"
-tit_font_size = round(120*scale)
+tit_font_size = round(120)
 
 lab_font      = "Harry P"
-lab_font_size = round(60*scale)
+lab_font_size = round(60)
 
 sco_font      = "Seven Segment"
-sco_font_size = round(140*scale)
+sco_font_size = round(130)
 
 tim_font      = "Seven Segment"
-tim_font_size = round(120*scale)
+tim_font_size = round(120)
+
+title = "JPX"
 
 # gloabal vars
 score_checked = 0
 timer_set     = 0
 blinking      = 0
 timing        = 0
+first_server  = "away"
+
+# load all the wave files
+use_wav = True
+use_explicit = True
+
+wav_321go    = "audio/321go.wav"
+wav_easports = "audio/EASports.wav"
+wav_nice     = "audio/nice.wav"
+if(use_explicit):
+  wav_stop     = "audio/stop_drinking_explicit.wav"
+else:
+  wav_stop     = "audio/stop_drinking.wav"
 
 #function to build the scoreboard in Tkinter
 def make_scoreboard(root):
-  print('making scoreboard')
   main_frame    = Frame(root,bg="black")
 
-  Label(main_frame,text="JPX",anchor="center",font=(tit_font,tit_font_size),fg='white',bg="black",width=8).grid(row=0,column=1,columnspan=2)
+  # scale factor for displaying on different sized screens
+  pixel_width = root.winfo_screenwidth()
+  scale = pixel_width/1100
 
-  Label(main_frame,text="AWAY",font=(lab_font,lab_font_size),fg='white',bg="black",width=8).grid(row=1,column=0)
-  away = Label(main_frame,text="0",font=(sco_font,sco_font_size),fg='orange',bg="black",width=3)
+
+  Label(main_frame,text=title,anchor="center",font=(tit_font,int(scale*tit_font_size)),fg='white',bg="black",width=9).grid(row=0,column=1,columnspan=2)
+
+  Label(main_frame,text="AWAY",font=(lab_font,int(scale*lab_font_size)),fg='white',bg="black",width=8).grid(row=1,column=0)
+  away = Label(main_frame,text="0",font=(sco_font,int(scale*sco_font_size)),fg='orange',bg="black",width=3)
   away.grid(row=2,column=0)
 
-  Label(main_frame,text="HOME",font=(lab_font,lab_font_size),fg='white',bg="black",width=8).grid(row=1,column=4)
-  home = Label(main_frame,text="0",font=(sco_font,sco_font_size),fg='green',bg="black",width=3)
+  Label(main_frame,text="HOME",font=(lab_font,int(lab_font_size*scale)),fg='white',bg="black",width=8).grid(row=1,column=4)
+  home = Label(main_frame,text="0",font=(sco_font,int(scale*sco_font_size)),fg='green',bg="black",width=3)
   home.grid(row=2,column=4)
   
-  timer = Label(main_frame,text="<-  ",font=(tim_font,tim_font_size),fg='red',bg="black",width=5)
+  timer = Label(main_frame,text="<-  ",font=(tim_font,int(tim_font_size*scale)),fg='red',bg="black",width=5)
   timer.grid(rowspan=2,columnspan=2,row=1,column=1)
   
   main_frame.pack(fill=X, padx=5, pady=5)
@@ -63,10 +80,7 @@ def sub_lab(lab):
   lab["text"] = str(int(score)-1)
 
 def set_timer(seconds):
-  if(seconds == 5):
-    timer["text"] = "5.0"
-  else:
-    timer["text"] = "2.0"
+  timer["text"] = f"{seconds:0.1f}"
   
 def run_timer():
   global timer_set
@@ -74,8 +88,11 @@ def run_timer():
   timing = 1
   seconds = float(timer["text"])
   time_left = seconds
-  audio.say("3...2...1...GO")
-  audio.runAndWait()
+  if(use_wav):
+    playsound(wav_321go)
+  else:
+    audio.say("3...2...1...GO")
+    audio.runAndWait()
   end = time.time() + seconds
   while(time_left > 0):
       time_left = float(int(math.ceil((end - time.time())*10)))/10
@@ -84,11 +101,27 @@ def run_timer():
         remaining += "0"
       timer["text"] = remaining
       root.update()
-  audio.say("Stop Drinking, Bitch!")
-  audio.runAndWait()
+  if(use_wav):
+    playsound(wav_stop)
+  else:
+    audio.say("Stop Drinking, Bitch!")
+    audio.runAndWait()
   timer_set = 0
   timing = 0
   
+def set_serve(team="away"):
+  if(team == "home"):
+    timer["text"] = "  ->"
+  else:
+    timer["text"] = "<-  "  
+  
+def get_serve():
+  server = None
+  if(timer["text"] == "  ->"):
+    server = "home"
+  else:
+    server = "away"
+  return server
   
   
 def change_serve():
@@ -130,6 +163,7 @@ def score_update(event):
   global timer_set
   global blinking
   global timing
+  global first_server
   
   button = event.char
   
@@ -138,17 +172,21 @@ def score_update(event):
     home_score = int(home["text"])
     away_score = int(away["text"])
     
-    if(timer["text"] == "5.0" or timer["text"] == "2.0"):
+    if(timer["text"] == "5.0" or timer["text"] == "2.0" or timer["text"] == "1.0" or timer["text"] == "1.5"):
       if(timer["text"] == "5.0"):
         home_score = 0
-        away_score = 0     
+        away_score = 0
       run_timer()
       home["text"] = str(home_score)
       away["text"] = str(away_score)
-      change_serve()
+      set_serve(first_server)
       
       
     else:
+   
+      # save state of server
+      if(home_score == 0 and away_score == 0):
+        first_server = get_serve()
    
       if(button == "7" and away_score < 99):
         score_checked = 0
@@ -172,25 +210,38 @@ def score_update(event):
         if((home_score+away_score) % 2 == 0 or (home_score+away_score) > 20 ):
           change_serve()
         home_score -= 1
-          
-
+        
+      # exit
+      elif(button == "-"):
+          root.destroy()
+          exit()
 
       elif(button == "2"):
         set_timer(2)
+        timer_set = 1
+        
+      elif(button == "="):
+        set_timer(1)
+        timer_set = 1
+        
+      elif(button == "+"):
+        set_timer(1.5)
         timer_set = 1
 
       #change serve
       elif(button == "8"):
           change_serve()
-          
       
       home["text"] = str(home_score)
       away["text"] = str(away_score)
       root.update()
       
       if(score_checked == 0 and ((home_score == 9 and away_score == 6) or (home_score == 6 and away_score == 9))):
-        audio.say("Nice!")
-        audio.runAndWait()
+        if(use_wav):
+          playsound(wav_nice)
+        else:
+          audio.say("Nice!")
+          audio.runAndWait()
         score_checked = 1
 
       if(home_score > winning_score-1 or away_score > winning_score-1):
@@ -210,17 +261,50 @@ def score_update(event):
 
 #Main function
 if __name__ == "__main__":
+
+  if(use_wav):
+    # Create the ea sports window window
+    window = Tk()
+    window.attributes('-fullscreen', True)
+    window.title("EA Sports")
+    
+    screen_width = window.winfo_screenwidth()
+    screen_height = window.winfo_screenheight()
+
+    # Load the image using Pillow
+    image = Image.open("images/ea_sports.jpg")  # Replace with your image path
+    image = image.resize((min(screen_width,screen_height), min(screen_width,screen_height)))
+    photo = ImageTk.PhotoImage(image)
+
+    # Create a label to display the image
+    image_label = Label(window, image=photo)
+    image_label.pack()
+
+    # Keep a reference to the image object to prevent garbage collection
+    image_label.image = photo
+
+    # Run the Tkinter event loop
+    window.after(250, lambda: playsound(wav_easports))
+    window.after(4000, lambda: window.destroy())
+    window.mainloop()
+  else:
+    audio = pyttsx3.init()
+    voices = audio.getProperty('voices')
+    audio.setProperty('voice', voices[1].id)
+
+
+  # make real scoreboard 
   root = Tk()
   root.title("JPX Scoreboard")
-  root.configure(bg="black")
-  audio = pyttsx3.init()
-  voices = audio.getProperty('voices')
-  audio.setProperty('voice', voices[1].id)
+  root.attributes('-fullscreen', True)
+  root.configure(bg="black")  
 
+  # put together scoreboard 
   [home, away, timer] = make_scoreboard(root)
-
-
   root.bind("<Key>",score_update)
+  
+  # make window capture key strokes
+  root.focus_force()
   
   #Make countdown timer
   countdown = Label(root,text="",font =(lab_font,48),fg="white",bg="black",anchor="center")
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # jpx_scoreboard_pc
+
+### Run Scoreboard
+navigate to directory and run `python JPX_scoreboard_pc.py`
+
+### Dependencies
+Install these python dependencies using pip
+- `PIL`
+- `pyttsx3`
+- `playsound==1.2.2`
+- `tkinter (apt-get python-tk on Linux...standard on windows)`
+
+`python -m pip install pillow pyttsx3 playsound==1.2.2`
+
+### Note from the author
+I did a truly awful job commenting anything so i apologize if you're trying to understand the code at all
